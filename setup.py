67044c1b6fb292e3f2398395c435325e5319e3c5
Alexandre Andrade <kaniabi@gmail.com>
2017-03-13 08:38:24 -0300

    Removing specific version requirements for jenkins-job-builder.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=[
         'zerotk.zops',
         'zerotk.jenkins-job-builder-pipeline==0.1.0',
-        'jenkins-job-builder==2.0.0.0b1',
+        'jenkins-job-builder',
     ],
     dependency_links=[],
     setup_requires=['setuptools_scm'],
